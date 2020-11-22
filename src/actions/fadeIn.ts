5b23ea59e6e383c639d00803d2c53312be1d5f4a
Nathan Klundt <nathan@blockchainjedi.com>
2020-11-22 11:54:38 -0700

    fix fade in animation

--- src/actions/fadeIn.ts
+++ src/actions/fadeIn.ts
@@ -14,7 +14,6 @@ export function getFadeInAction(
         renderFlags.renderOverrideFlag = false;
       }
       if ( n === 60 ) {
-        // to 48
         renderFlags.renderOverrideFlag = true;
         imgData = altCtx.getImageData(
           0,
@@ -26,6 +25,29 @@ export function getFadeInAction(
         for ( let i = 0; i < imgData.data.length; i += 4 ) {
           switch ( imgData.data[i] ) {
             case 155:
+              imgData2.data[i + 0] = 48;
+              imgData2.data[i + 1] = 98;
+              imgData2.data[i + 2] = 48;
+              imgData2.data[i + 3] = 255;
+              break;
+            default:
+              imgData2.data[i + 0] = 15;
+              imgData2.data[i + 1] = 56;
+              imgData2.data[i + 2] = 15;
+              imgData2.data[i + 3] = imgData.data[i + 3];
+          }
+        }
+        ctx.putImageData( imgData2, 0, 0 );
+      }
+      if ( n === 40 ) {
+        for ( let i = 0; i < imgData.data.length; i += 4 ) {
+          switch ( imgData.data[i] ) {
+            case 155:
+              imgData2.data[i + 0] = 139;
+              imgData2.data[i + 1] = 172;
+              imgData2.data[i + 2] = 15;
+              imgData2.data[i + 3] = 255;
+              break;
             case 139:
               imgData2.data[i + 0] = 48;
               imgData2.data[i + 1] = 98;
@@ -33,25 +55,11 @@ export function getFadeInAction(
               imgData2.data[i + 3] = 255;
               break;
             default:
-              imgData2.data[i + 0] = imgData.data[i + 0];
-              imgData2.data[i + 1] = imgData.data[i + 1];
-              imgData2.data[i + 2] = imgData.data[i + 2];
+              imgData2.data[i + 0] = 15;
+              imgData2.data[i + 1] = 56;
+              imgData2.data[i + 2] = 15;
               imgData2.data[i + 3] = imgData.data[i + 3];
-          }
-        }
-        ctx.putImageData( imgData2, 0, 0 );
-      }
-      if ( n === 40 ) {
-        // to 139
-        for ( let i = 0; i < imgData.data.length; i += 4 ) {
-          if ( imgData.data[i] === 155 ) {
-            imgData2.data[i + 0] = 139;
-            imgData2.data[i + 1] = 172;
-            imgData2.data[i + 2] = 15;
-          } else {
-            imgData2.data[i + 0] = imgData.data[i + 0];
-            imgData2.data[i + 1] = imgData.data[i + 1];
-            imgData2.data[i + 2] = imgData.data[i + 2];
+
           }
         }
         ctx.putImageData( imgData2, 0, 0 );
